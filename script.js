13d2a2774b027dff0eb216beca7ac07f8abd6af4
octubre1100 <136340911+octubre1100@users.noreply.github.com>
2025-06-04 20:56:19 -0300

    epico

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
 let butonR = document.getElementById("reg");
 let butonA = document.querySelector("#agro");
 let butonC = document.querySelector("#canc");
-let pam = document.querySelector(".pam");
-let cuyo = document.querySelector(".cuyo");
-let nea = document.querySelector(".nea");
-let noa = document.querySelector(".noa");
-let pat = document.querySelector(".pat");
+let pam = document.querySelectorAll(".pam");
+let cuyo = document.querySelectorAll(".cuyo");
+let nea = document.querySelectorAll(".nea");
+let noa = document.querySelectorAll(".noa");
+let pat = document.querySelectorAll(".pat");
 let regi = false;
 butonR.onclick = function () {
-  console.log("anda");
   if (regi == false) {
-    pam.classList.toggle("act");
-    pat.classList.toggle("act");
-    cuyo.classList.toggle("act");
-    nea.classList.toggle("act");
-    noa.classList.toggle("act");
+    pam.forEach((el) => el.classList.toggle("A"));
+    pat.forEach((el) => el.classList.toggle("E"));
+    cuyo.forEach((el) => el.classList.toggle("I"));
+    nea.forEach((el) => el.classList.toggle("O"));
+    noa.forEach((el) => el.classList.toggle("U"));
     regi = true;
     console.log("anda");
   }
-  console.log("anda");
 };
