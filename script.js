829ef941b9b208cc53d2a28c6ea7ffe1efb58a6a
octubre1100 <136340911+octubre1100@users.noreply.github.com>
2025-06-04 21:43:29 -0300

    loco

--- script.js
+++ script.js
@@ -8,6 +8,14 @@ let noa = document.querySelectorAll(".noa");
 let pat = document.querySelectorAll(".pat");
 let regi = false;
 butonR.onclick = function () {
+  if (agro == true) {
+    muyalto.forEach((el) => el.classList.toggle("Q"));
+    alto.forEach((el) => el.classList.toggle("W"));
+    medio.forEach((el) => el.classList.toggle("R"));
+    bajo.forEach((el) => el.classList.toggle("T"));
+    nulo.forEach((el) => el.classList.toggle("Y"));
+    agro = false;
+  }
   if (regi == false) {
     pam.forEach((el) => el.classList.toggle("A"));
     pat.forEach((el) => el.classList.toggle("E"));
@@ -15,6 +23,32 @@ butonR.onclick = function () {
     nea.forEach((el) => el.classList.toggle("O"));
     noa.forEach((el) => el.classList.toggle("U"));
     regi = true;
+    console.log("anda");
+  }
+};
+let muyalto = document.querySelectorAll(".muyalto");
+let alto = document.querySelectorAll(".alto");
+let medio = document.querySelectorAll(".medio");
+let bajo = document.querySelectorAll(".bajo");
+let nulo = document.querySelectorAll(".nulo");
+let agro = false;
+butonA.onclick = function () {
+  if (regi == true) {
+    pam.forEach((el) => el.classList.toggle("A"));
+    pat.forEach((el) => el.classList.toggle("E"));
+    cuyo.forEach((el) => el.classList.toggle("I"));
+    nea.forEach((el) => el.classList.toggle("O"));
+    noa.forEach((el) => el.classList.toggle("U"));
+    console.log("anda");
+    regi = false;
+  }
+  if (agro == false) {
+    muyalto.forEach((el) => el.classList.toggle("Q"));
+    alto.forEach((el) => el.classList.toggle("W"));
+    medio.forEach((el) => el.classList.toggle("R"));
+    bajo.forEach((el) => el.classList.toggle("T"));
+    nulo.forEach((el) => el.classList.toggle("Y"));
+    agro = true;
     console.log("anda");
   }
 };
